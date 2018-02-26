44bf7fb80f62f8e88177633905bb2881e2e014c3
mac <mac@macs-MacBook-Pro.local>
2018-02-26 18:40:31 +0000

    Versão quase final. Ficam a faltar os 4 pontos que deixei no Asana

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,34 +1,55 @@
 import jQuery from 'jquery';
 import anime from 'animejs';
-//import scrollNavigation from './core/scrollNavigation';
+
+/*------------------------------------*\
+    #MORPHO ANIM.JS
+\*------------------------------------*/
+//based on Anime.js ref: http://animejs.com/documentation/#morphing
+const morphingSVG = (svgPath) => {
+  anime({
+    targets: svgPath,
+    duration: 4000,
+    easing: [0.5,0,0.5,1],
+    d: [
+        { value: 'M83.5,536.9c0-247.5,174.5-480,422-480s485,229.5,485,477s-185.5,429-433,429S83.5,784.4,83.5,536.9z'},
+        { value: 'M96,492.9c0-247.5,163.5-459,411-459s471,179.5,471,427s-184.5,525-432,525S96,740.4,96,492.9z'},
+        { value: 'M83.5,536.9c0-247.5,174.5-480,422-480s485,229.5,485,477s-185.5,429-433,429S83.5,784.4,83.5,536.9z'}
+    ],
+    loop: true
+  });
+
+};
+
+morphingSVG(document.getElementById('js-svg-clip'));
 
 
-/******************* MENU *******************/
+/*------------------------------------*\
+    #MENU
+\*------------------------------------*/
 let menu =  document.getElementById('js-menu');
+let title = document.getElementById('js-title');
+let hamburguerBtn = document.getElementById('js-menu-opener');
 /**
  * check if is mobile device based on media queries
  */
 const isMobile = () => window.matchMedia( "(max-width: 64rem)" ).matches;
 if (!isMobile()) {
-    //do something for desktop
-    menu.classList.remove('is--hidden');
-    menu.classList.add('is--visible');
+    // for desktop:
+    title.classList.remove('is--visible');
+    title.classList.add('is--hidden'); 
+    hamburguerBtn.classList.add('is--hidden');
+    hamburguerBtn.classList.remove('is--visible');
 }
 else {
+    // for mobile:
     menu.classList.add('is--hidden');
     menu.classList.remove('is--visible');
+    title.classList.add('is--visible');
+    title.classList.remove('is--hidden'); 
 }
 
 // OPEN MENU BTN //
-let hamburguerBtn = document.getElementById('js-menu-opener');
-//let menu =  document.getElementById('js-menu');
 hamburguerBtn.addEventListener('click', function() {
-    /*anime({
-        targets: menu,
-        left: 0,
-        easing: 'easeOutExpo'
-    });*/
-
     if (menu.classList.contains('is--hidden')) {
         menu.classList.remove('is--hidden');
         menu.classList.add('is--visible');
@@ -40,103 +61,69 @@ hamburguerBtn.addEventListener('click', function() {
 
 window.$ = jQuery;
 
-
 // CLOSE MENU BTN //
 hamburguerBtn.addEventListener('click', function() {
     menu.classList.toggle('active');
     hamburguerBtn.classList.toggle('active');
 }, false);
 
-
-
-// CLOSE MENU WHEN SELECTING MENU-LINK //
-let menuLink = document.getElementById('js-link');
-menuLink.addEventListener('click', function() {
-    console.log('lala');
-    menu.classList.add('is--hidden');
-    menu.classList.remove('is--visible');
-});
-
-/*
-$(document).ready(function() {
-   $(document).on('click', '#js-menu-opener', function() {
-        $(this).toggleClass('active');
-        $(".menu").toggleClass("active");
-    });
-});
-*/
 const blocks = document.querySelectorAll('.js-block');
 const links = document.querySelectorAll('.menu-links');
 
 blocks.forEach(function(block) {
     let offsetBlock = block.getBoundingClientRect();
     console.log(offsetBlock.y);
-
-    //links.classList.remove('is--active');
-    //let link = block.dataset.link();
-    //link.classlist.add('is--active');
 });
 
 window.addEventListener('scroll', function() {
     
 });
-/*
-$(document).ready(function() {
 
-    $(document).on('click', '.menu-opener', function() {
+/*------------------------------------*\
+    #Scroll to section
+\*------------------------------------*/
+const scrollToSection = (el) => {
+    let {top} = el.getBoundingClientRect();
+    let mainHeaderHeight = document.getElementById('top').scrollHeight;
+  
+    const scroll = {
+        y: window.pageYOffset
+    };
 
+    anime({
+        targets: scroll,
+        y: ((top + window.pageYOffset) - mainHeaderHeight),
+        duration: 850,
+        easing: 'easeInOutExpo',
+        update: () => window.scroll(0, scroll.y)
     })
-    $(".menu-opener").click(function() {
-      $(".menu-opener, .menu").toggleClass("active");
-    });
+}
 
-        $('.menu-link').on('click', function() {
-            var _this = $(this);
-            var section = $(_this).attr('href');
-            var sectionFromTop = $(section).offset().top;
-            $('html,body').animate({
-                scrollTop: sectionFromTop,
-            }, 800);
-            if (window.innerWidth <= 768) {
-                $(document).find('.menu-opener').trigger('click');
-            }
-            return false;
-        });
+const navigate = (el) => {
+    el.addEventListener('click', () => {
+        let anchorSection = el.dataset.section; 
+        scrollToSection(document.getElementById(anchorSection));
+        menu.classList.add('is--hidden');
+        menu.classList.remove('is--visible');
+        hamburguerBtn.classList.toggle('active');
+    }, false);
+}
 
-});
-*/
-
-/*let hamburguerBtn = document.getElementById('js-menu-opener');
-let menu =  document.getElementById('js-menu');
-*/
-
-
-
-
-/*function initMap() {
-    var map; 
-   
-    map = new google.maps.Map(document.getElementById('map'), {
-
-        center: {lat: 38.6954972, lng: -9.219691},
-        zoom: 15,
-        disableDefaultUI: true
-    });
-    var marker = new google.maps.Marker({
-        position: {lat: 38.6954972, lng: -9.219691},
-        map: map,
-        title: 'CCQ',
-        icon: {
-        path: google.maps.SymbolPath.CIRCLE,  
-        scale:15  
-        }
+// Add click event to links //
+const scrollNavigation = (navLinks) => {
+    navLinks.forEach((link) => {
+        navigate(link);
     });
 }
-*/
-//window.onload = initMap();
+
+// Main Navigation scroll animation //
+const navLinks = document.querySelectorAll('.menu-link');
+scrollNavigation(navLinks);
 
 
-/******************* MAP STYLE *******************/
+/*------------------------------------*\
+    #MAP STYLE
+\*------------------------------------*/
 var styles = [
     {
         //nomes bairros
@@ -329,8 +316,11 @@ var styles = [
     }
 ]
 
-/******************* MAP & CONTACT BOX *******************/
+/*------------------------------------*\
+    #MAP & CONTACT BOX
+\*------------------------------------*/
 let box = document.getElementById('js-box');
+let close = document.getElementById('js-close');
 
 function initMap(el) {
     let map = new google.maps.Map(document.getElementById(el), {
@@ -365,85 +355,9 @@ function initMap(el) {
         toggleBox(box);
     });
 
-    box.addEventListener('click', function() {
+    close.addEventListener('click', function() {
         toggleBox(box);
     });
-
 }
-
-//initMap('map');
 
 initMap('js-map-2');
-
-
-
-/**
- * Animate SVG path
- * based on Anime.js ref: http://animejs.com/documentation/#morphing
-*/
-const morphingSVG = (svgPath) => {
-  /*
-   * TODO: grab the values from the other shapes in illustrator
-   * and replace string values
-   */
-
-  anime({
-    targets: svgPath,
-    duration: 10000,
-    easing: [0.5,0,0.5,1],
-    d: [
-      { value: 'M485.4,55.4c132.7,0,243.6,46.2,326.7,127s142,174.3,137.4,288.6S914.9,318,841,706.8S638.6,945,408.3,944.6 c-130.3-0.4-241.1-30.4-338-110.1S50.3,614.1,50.3,283.6s64.6-244.7,154.7-322.1S352.7,55.4,485.4,55.4z'},
-      {value: 'M485.4,55.4c132.7,0,243.6,46.2,326.7,127s142,174.3,137.4,288.6S914.9,718,841,806.8S638.6,945,508.3,944.6 c-130.3-0.4-241.1-30.4-338-110.1S50.3,614.1,50.3,483.6s64.6-244.7,154.7-322.1S352.7,55.4,485.4,55.4z'}
-    ],
-    loop: true
-  });
-
-};
-
-morphingSVG(document.getElementById('js-svg-clip'));
-
-
-/**
- * Scroll to section
- */
-const scrollToSection = (el) => {
-  let {top} = el.getBoundingClientRect();
-  let mainHeaderHeight = document.getElementById('top').scrollHeight;
-  
-  const scroll = {
-    y: window.pageYOffset
-  };
-
-  anime({
-    targets: scroll,
-    y: ((top + window.pageYOffset) - mainHeaderHeight),
-    duration: 850,
-    easing: 'easeInOutExpo',
-    update: () => window.scroll(0, scroll.y)
-  })
-}
-
-const navigate = (el) => {
-    el.addEventListener('click', () => {
-        let anchorSection = el.dataset.section; 
-        scrollToSection(document.getElementById(anchorSection));
-    }, false);
-}
-
-/**
- * Add click event to links 
- * and attach scroll function
- */
-const scrollNavigation = (navLinks) => {
-    navLinks.forEach((link) => {
-        navigate(link);
-    });
-}
-
-/**
- * Main Navigation scroll animation
- * see ./core/scrollNavigation.js for details
- */
-const navLinks = document.querySelectorAll('.menu-link');
-scrollNavigation(navLinks);
-
